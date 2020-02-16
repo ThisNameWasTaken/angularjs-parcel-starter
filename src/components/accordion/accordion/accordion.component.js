77ebbc4bffae9c3a5b7276fb60fe350c8bbab9f8
Adrian <adriandinca2008@yahoo.com>
2020-02-17 00:55:09 +0200

    feat: Calculate border height

--- src/components/accordion/accordion/accordion.component.js
+++ src/components/accordion/accordion/accordion.component.js
@@ -14,12 +14,47 @@ angular.module('accordion.component').component('appAccordion', {
       /** @type {angular.IRootElementService} */
       $element
     ) {
+      /**
+       *
+       * @param {Element} expansionPanel
+       */
+      function findParentExpansionPanel(expansionPanel) {
+        let parentExpansionPanel = expansionPanel.parentElement;
+
+        while (
+          !parentExpansionPanel.classList.contains('expansion-panel') &&
+          !parentExpansionPanel.classList.contains('accordion')
+        ) {
+          parentExpansionPanel = parentExpansionPanel.parentElement;
+        }
+
+        return parentExpansionPanel;
+      }
+
+      const resizeObserver = new ResizeObserver(entries => {
+        entries.forEach(entry => {
+          const lastExpansionPanels = Array.from(
+            entry.target.querySelectorAll('.expansion-panel:last-child')
+          );
+
+          lastExpansionPanels.forEach(panel => {
+            const parentPanel = findParentExpansionPanel(panel);
+
+            parentPanel.style.setProperty(
+              '--expansion-panel-border-height',
+              `${panel.getBoundingClientRect().top -
+                parentPanel.getBoundingClientRect().top}px`
+            );
+          });
+        });
+      });
+
       this.$onInit = function() {
-        console.log('Init');
+        resizeObserver.observe($element.children()[0]);
       };
 
       this.$onDestroy = function() {
-        console.log('Destroy');
+        resizeObserver.disconnect();
       };
     },
   ],
